3d81bcb752dda106b0dccafdec1a395408a4997f
EvgenSharapov <dooplert@yandex.ru>
2024-12-10 11:37:39 +0300

    22

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import entity.Book;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book=new Book("erfg","ergerg");
+
     }
 }
